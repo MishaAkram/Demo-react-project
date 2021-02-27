3c9233c09dc2c2af423e3178ced7d99577b27943
MishaAkram <mishaakrambaig@gmail.com>
2021-02-27 18:27:04 +0500

    http get request implemented

--- react-http-requests/src/components/PostList.js
+++ react-http-requests/src/components/PostList.js
@@ -5,17 +5,37 @@ export class PostList extends Component {
         super(props)
     
         this.state = {
-             posts: []
+             posts: [],
+             error
         }
     }
       componentDidMount(){
-          axios.get()
+          axios.get('https://jsonplaceholder.typicode.com/posts')
+          .then(response =>
+            {
+              console.log(response)
+              this.setState({posts: response.data
+            })
+          })
+          .catch(error =>
+            {
+              console.log(error)
+              this.setState({errorMsg:'error retreving data'})
+          })
       }
     
     render() {
+        const  {posts,errorMsg} =this.state
         return (
             <div>
-                
+                list of Post lists
+                {
+                 posts.length ? 
+                 posts.map(post => <div key= {post.id}>{post.title}</div>)
+                :
+                null
+                }
+                {errorMsg ? <div>{errorMsg}</div> : null} 
             </div>
         )
     }
