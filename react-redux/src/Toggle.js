5fc525f80205830df6de1f7c2314ac6257fa891c
MishaAkram <mishaakrambaig@gmail.com>
2021-04-16 10:48:11 +0500

    refactored class component to functional component

--- react-redux/src/Toggle.js
+++ react-redux/src/Toggle.js
@@ -2,27 +2,24 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { toggleSwitch } from "./UiReducer";
 
-class Toggle extends Component {
-  render() {
-    const { ui, toggleSwitch } = this.props;
-    return (
-      <div>
+const Toggle = ({ ui, toggleSwitch }) => (
+    <div>
         <div>{JSON.stringify(ui)}</div>
         <input
-          type="checkbox"
-          value={ui.toggle}
-          onChange={toggleSwitch}
+            type="checkbox"
+            value={ui.toggle}
+            onChange={toggleSwitch}
         />
-      </div>
-    );
-  }
-}
+    </div>
+);
+
+
 
 const mapStateToProps = ({ ui }) => ({
-  ui
+    ui
 });
 
 export default connect(
-  mapStateToProps,
-  { toggleSwitch }
+    mapStateToProps,
+    { toggleSwitch }
 )(Toggle);
